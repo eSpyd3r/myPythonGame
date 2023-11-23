657a93d27d91ed5cbc2f5417bbf8154912d81ce3
eSpyd3r <98184622+eSpyd3r@users.noreply.github.com>
2023-11-23 17:20:03 -0500

    Added weapon.py via upload and animations
    Added animations to player and attacks, implemented weapon graphics

--- level.py
+++ level.py
@@ -5,6 +5,7 @@ from player import Player
 from debug import debug
 from support import *
 from random import choice
+from weapon import Weapon
 
 class Level:
 	def __init__(self):
@@ -16,8 +17,10 @@ class Level:
 		self.visible_sprites = YSortCameraGroup()
 		self.obstacle_sprites = pygame.sprite.Group()
 
+		#attack sprites
+		self.current_attack = None
+
 		#sprite setup
-	
 		self.create_map()
 
 	def create_map(self):
@@ -51,13 +54,22 @@ class Level:
 		#			Tile((x,y),[self.visible_sprites,self.obstacle_sprites])
 		#		if col == 'p':
 		#			self.player = Player((x,y),[self.visible_sprites],self.obstacle_sprites) 
-		self.player = Player((2000,1430),[self.visible_sprites],self.obstacle_sprites) 
+		self.player = Player((2000,1430),[self.visible_sprites],self.obstacle_sprites,self.create_attack,self.destroy_attack) 
 
+	def create_attack(self):
+		self.current_attack = Weapon(self.player,[self.visible_sprites])
+
+	def destroy_attack(self):
+		if self.current_attack:
+			self.current_attack.kill()
+		self.current_attack = None
 
 	def run(self):
 		#update/draw game
 		self.visible_sprites.custom_draw(self.player)
 		self.visible_sprites.update()
+
+		debug(self.player.status)
 
 class YSortCameraGroup(pygame.sprite.Group):
 	def __init__(self):
--- player.py
+++ player.py
@@ -1,34 +1,106 @@
 import pygame
 from settings import *
+from support import import_folder
 
 class Player(pygame.sprite.Sprite):
-	def __init__(self,pos,groups,obstacle_sprites):
+	def __init__(self,pos,groups,obstacle_sprites,create_attack,destroy_attack):
 		super().__init__(groups)
 		self.image = pygame.image.load('../graphics/test/player.png').convert_alpha()
 		self.rect = self.image.get_rect(topleft = pos)
 		self.hitbox = self.rect.inflate(0,-26)
 
+		#graphics setup
+		self.import_player_assets()
+		self.status = 'down'
+		self.frame_index = 0
+		self.animation_speed = 0.15
+
+		#movement
 		self.direction = pygame.math.Vector2()
 		self.speed = 5
+		self.attacking = False
+		self.attack_cooldown = 400
+		self.attack_time = None
+
+		#weapon
+		self.create_attack = create_attack
+		self.destroy_attack = destroy_attack
+		self.weapon_index = 0
+		self.weapon = list(weapon_data.keys())[self.weapon_index]
+
 
 		self.obstacle_sprites = obstacle_sprites
+
+	def import_player_assets(self):
+		character_path = '../graphics/player/'
+		self.animations = {
+		'up': [], 'down': [], 'left': [], 'right': [], 
+		'right_idle': [], 'left_idle': [],
+		'up_idle': [], 'down_idle': [], 
+		'right_attack': [], 'left_attack': [], 
+		'up_attack': [], 'down_attack': []
+		}
+
+		for animation in self.animations.keys():
+			full_path = character_path + animation
+			self.animations[animation] = import_folder(full_path)
+		#print(self.animations)
 
 	def input(self):
 		keys = pygame.key.get_pressed()
 
-		if keys[pygame.K_UP]:
-			self.direction.y = -1
-		elif keys[pygame.K_DOWN]:
-			self.direction.y = 1
-		else:
-			self.direction.y = 0
+		if not self.attacking:
+			#movement
+			if keys[pygame.K_UP]:
+				self.direction.y = -1
+				self.status = 'up'
+			elif keys[pygame.K_DOWN]:
+				self.direction.y = 1
+				self.status = 'down'
+			else:
+				self.direction.y = 0
 
-		if keys[pygame.K_RIGHT]:
-			self.direction.x = 1
-		elif keys[pygame.K_LEFT]:
-			self.direction.x = -1
-		else:
+			if keys[pygame.K_RIGHT]:
+				self.direction.x = 1
+				self.status = 'right'
+			elif keys[pygame.K_LEFT]:
+				self.direction.x = -1
+				self.status = 'left'
+			else:
+				self.direction.x = 0
+
+			#attack
+			if keys[pygame.K_SPACE]:
+				self.attacking = True
+				self.attack_time = pygame.time.get_ticks()
+				self.create_attack()
+				
+
+			#magic
+			if keys[pygame.K_LCTRL]:
+				self.attack_time = pygame.time.get_ticks()
+				self.attacking = True
+
+	def get_status(self):
+
+		#idle
+		if self.direction.x == 0 and self.direction.y == 0:
+			if not 'idle' in self.status and not 'attack' in self.status:
+				self.status = self.status + '_idle'
+
+		if self.attacking:
 			self.direction.x = 0
+			self.direction.y = 0
+			if not 'attack' in self.status:
+				if 'idle' in self.status:
+					#override idle
+					self.status = self.status.replace('_idle','_attack')
+				else:
+					self.status = self.status + '_attack'
+
+		else:
+			if 'attack' in self.status:
+				self.status = self.status.replace('_attack','')
 
 	def move(self,speed):
 		if self.direction.magnitude() != 0: #vectors of length 0 can't be normalized
@@ -41,7 +113,6 @@ class Player(pygame.sprite.Sprite):
 		self.collision('vertical')
 		self.rect.center = self.hitbox.center
 		
-
 	def collision(self,direction):
 		if direction == 'horizontal':
 			for sprite in self.obstacle_sprites:
@@ -59,6 +130,30 @@ class Player(pygame.sprite.Sprite):
 					if self.direction.y < 0: #in motion upwards
 						self.hitbox.top = sprite.hitbox.bottom
 
+	def cooldowns(self):
+		current_time = pygame.time.get_ticks()
+
+		if self.attacking:
+			if current_time - self.attack_time >= self.attack_cooldown:
+				self.attacking = False
+				self.destroy_attack()
+
+	def animate(self):
+		animation = self.animations[self.status]
+
+		#loop over frame index
+		self.frame_index += self.animation_speed
+		if self.frame_index >= len(animation):
+			self.frame_index = 0
+
+		#set image
+		self.image = animation[int(self.frame_index)]
+		self.rect = self.image.get_rect(center = self.hitbox.center)
+
+
 	def update(self):
 		self.input()
+		self.cooldowns()
+		self.get_status()
+		self.animate()
 		self.move(self.speed)
