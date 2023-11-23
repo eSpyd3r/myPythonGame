4de86660aff95e8036834e0393c02a478ed253d7
eSpyd3r <98184622+eSpyd3r@users.noreply.github.com>
2023-11-23 02:30:06 -0500

    Updated Level, Added support.py via upload
    Added objects to world map and created support.py to read csv files for the level generation

--- level.py
+++ level.py
@@ -3,6 +3,8 @@ from settings import *
 from tile import Tile
 from player import Player
 from debug import debug
+from support import *
+from random import choice
 
 class Level:
 	def __init__(self):
@@ -19,10 +21,32 @@ class Level:
 		self.create_map()
 
 	def create_map(self):
-		#for row_index,row in enumerate(WORLD_MAP):
-		#	for col_index,col in enumerate(row):
-		#		x = col_index * TILESIZE
-		#		y = row_index * TILESIZE
+		layout = {
+				'boundary' : import_csv_layout('../map/map_FloorBlocks.csv'),
+				'grass' : import_csv_layout('../map/map_Grass.csv'),
+				'object': import_csv_layout('../map/map_Objects.csv'),
+		}
+		graphics = {
+				'grass': import_folder('../graphics/Grass'),
+				'objects': import_folder('../graphics/objects')
+		}
+
+
+		for style,layout in layout.items():
+			for row_index,row in enumerate(layout):
+				for col_index,col in enumerate(row):
+					if col != '-1':
+						x = col_index * TILESIZE
+						y = row_index * TILESIZE
+						if style == 'boundary':
+							Tile((x,y),[self.obstacle_sprites],'invisible')
+						if style == 'grass':
+							random_grass_image = choice(graphics['grass'])
+							Tile((x,y),[self.visible_sprites,self.obstacle_sprites],'grass', random_grass_image)
+
+						if style == 'object':
+							surf = graphics['objects'][int(col)]
+							Tile((x,y),[self.visible_sprites,self.obstacle_sprites],'object',surf)
 		#		if col == 'x':
 		#			Tile((x,y),[self.visible_sprites,self.obstacle_sprites])
 		#		if col == 'p':
